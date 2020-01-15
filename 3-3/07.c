7397d49b50200e7d5bf0ddb7eeb5938494550bd8
Sean Cushman <cushsean@gmail.com>
2020-01-15 17:03:51 -0500

    Daily commit

--- 3-3/07.c
+++ 3-3/07.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "list.h"
 #include "tree.h"
 
 int main(){
-	
-	tree_b* root = mkTree_bin(4);
-	ptTree_bin(root);
-	//printf("DEPTH: %d\n", get_depth_bin(root));
+	srand(time(NULL));
+	tree_b* root = mkTree_bst(15);
+	ptTree_bst(root);
+	int tmp = rand()%20;
+	printf("finding %d...\n", tmp);
+	tree_b* leaf = find_leaf_bst(root, tmp);
+	if(leaf != NULL)
+		printf("The node with value %d has children %d(l) and %d(r)\n",
+			leaf->data,
+			(leaf->left == NULL ? -1 : leaf->left->data),
+			(leaf->right == NULL ? -1 : leaf->right->data)
+		);
+	rmLeaf_bst(root, leaf->data);
+	ptTree_bst(root);
 	return 0;
 }
--- 3-3/tree.h
+++ 3-3/tree.h
@@ -14,22 +14,23 @@ typedef struct tree{
 	struct tree *child;
 }tree_t;
 
-typedef struct bin_tree{
+typedef struct bst_tree{
 	int data;
-	struct bin_tree *parent;
-	struct bin_tree *left;
-	struct bin_tree *right;
+	struct bst_tree *parent;
+	struct bst_tree *left;
+	struct bst_tree *right;
 }tree_b;
 
 tree_t* mkTree(int);
-tree_b* mkTree_bin(int);
+tree_b* mkTree_bst(int);
 void mkLeaf(tree_t*, int);
-tree_b* mkLeaf_bin(int);
-void insert_leaf_bin(tree_b*, int);
+tree_b* mkLeaf_bst(int);
+void insert_leaf_bst(tree_b**, int);
 void ptTree(tree_t*);
-void ptTree_bin(tree_b*);
-void ptTree_bin_util(tree_b*, int);
-int get_depth_bin(tree_b*);
+void ptTree_bst(tree_b*);
+void ptTree_bst_util(tree_b*, int);
+tree_b* find_min_bst(tree_b*);
+tree_b* find_max_bst(tree_b*);
 
 tree_t* mkTree(int num){
 	tree_t* root;
@@ -40,10 +41,11 @@ tree_t* mkTree(int num){
 	return root;
 }
 
-tree_b* mkTree_bin(int num){
+tree_b* mkTree_bst(int num){
+	srand(time(NULL));
 	tree_b* root = NULL;
 	for(int i = 0; i < num; i++)
-		insert_leaf_bin(root, i);
+		insert_leaf_bst(&root, rand()%20);
 	return root;
 }
 
@@ -52,34 +54,99 @@ void mkLeaf(tree_t* root, int value){
 	return;
 }
 
-tree_b* mkLeaf_bin(int value){
-	tree_b* leaf = (tree_b*)malloc(sizeof(tree_b*));
+tree_b* mkLeaf_bst(int value){
+	tree_b* leaf = (tree_b*)malloc(sizeof(tree_b));
 	leaf->left = NULL;
 	leaf->right = NULL;
-	leaf->parent = NULL;
 	leaf->data = value;
 	return leaf;
 }
 
-void insert_leaf_bin(tree_b* root, int value){
-	if(root == NULL)
-		root = mkLeaf_bin(value);
+void insert_leaf_bst(tree_b** root, int value){
+	if(*root == NULL){
+		*root = mkLeaf_bst(value);
 		return;
-	int cmp = cmp_int(value, root->data);
+	}
+	int cmp = cmp_int(value, (*root)->data);
 	switch(cmp){
 		case 1:
 			//value > root->data
-			return insert_leaf_bin(root->right, value);
+			return insert_leaf_bst(&(*root)->right, value);
 			break;
 		case -1:
 			//value < root->data
-			return insert_leaf_bin(root->left, value);
+			return insert_leaf_bst(&(*root)->left, value);
 			break;
 		default:
 			//value == root->data
 			printf("No leaf created.\n");
 			break;
 	}
+	return;
+}
+
+tree_b* find_leaf_bst(tree_b* root, int value){
+	tree_b* leaf = root;
+	if(leaf == NULL){
+		printf("Leaf not found, returned NULL\n");
+		return leaf;
+	}
+	int cmp = cmp_int(value, leaf->data);
+	if(cmp == 1)
+		return find_leaf_bst(leaf->right, value);
+	else if(cmp == -1)
+		return find_leaf_bst(leaf->left, value);
+	else{
+		return leaf;
+	}
+}
+
+void rmLeaf_bst(tree_b* root, int value){
+	//case 1: has no children
+	//case 2: has 1 child
+	//case 3: has 2 children
+	
+	//get parent && set root to node to be removed
+	tree_b* parent = NULL; 
+	tree_b* swap = NULL;
+	while(root->data != value){
+		parent = root;
+		if(value < root->data)
+			root = root->left;
+		else
+			root = root->right;
+		if(root == NULL){
+			/**************************************
+				SegFault when value is not in tree
+			***************************************/
+			printf("Value not found. No leaf removed.\n");
+			return;
+		}
+	}
+
+	if((root->left != NULL) != (root->right != NULL)){
+		//1 Child
+		if(root->left != NULL)
+			swap = root->left;
+		else if(root->right != NULL)
+			swap = root->right;
+	}
+	else if(root->left != NULL && root->right != NULL){
+		//2 Children
+		swap = find_min_bst(root);
+	}
+	//Any Children
+	if(parent != NULL){
+		if(parent->data > root->data)
+			parent->left = swap;
+		else
+			parent->right = swap;
+	}
+	else{
+
+	}
+	free(root);
+	
 	return;
 }
 
@@ -90,52 +157,40 @@ void ptTree(tree_t* root){
 	return;
 }
 
-void ptTree_bin(tree_b* root){
+void ptTree_bst(tree_b* root){
 	printf("\nPrinting Tree...");
-	ptTree_bin_util(root, 0);
+	ptTree_bst_util(root, 0);
 	return;
 }
 
-void ptTree_bin_util(tree_b* root, int num){	
+void ptTree_bst_util(tree_b* root, int num){	
 	if(root == NULL)
 		return;
 	
 	num += 1;
 	
-	ptTree_bin_util(root->right, num);
+	ptTree_bst_util(root->right, num);
 	
 	printf("\n");
 	for(int i=0; i<num; i++)
 		printf("\t");
 	printf("%d\n", root->data);
 	
-	ptTree_bin_util(root->left, num);
+	ptTree_bst_util(root->left, num);
 	
 	return;
 }
 
-int get_depth(){
-	int depth = 1;	
-	return depth;
+tree_b* find_min_bst(tree_b* root){
+	if(root->left == NULL)
+		return root;
+	return find_min_bst(root->left);
 }
 
-int get_depth_bin(tree_b* root){
-	tree_b* curr = root;
-	int depth = 0;
-	for(depth; curr->left != NULL; depth++, curr = curr->left);
-	return depth;
+tree_b* find_max_bst(tree_b* root){
+	if(root->right == NULL)
+		return root;
+	return find_max_bst(root->right);
 }
-
-
-/*
-tree_b* find_emptyLeaf(tree_b* root){
-	tree_b* parent = root;
-	if(root->left != NULL){
-		tree_b* child = root->left;
-	}
-	while(parent
-	return parent;
-}
-*/
 
 #endif /* TREE_H_CUSHMAN */
