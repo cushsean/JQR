0c61f271341d6740ee8bf271c45475b528d025ac
sean cushman <cushsean@gmail.com>
2020-04-30 12:33:29 -0400

    made changes to signals.c that were requested, ready for review

--- signals.c
+++ signals.c
@@ -3,11 +3,12 @@
 #include <signal.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 void handle_sigabrt(int sig){
 	if(sig == 6){
 		printf("ABORT is failure! Simply exit please.\n");
-		exit(1);
+		exit(EXIT_SUCCESS);
 	}
 }
 
@@ -16,15 +17,33 @@ void handle_sigint(int sig){
 	abort();
 }
 
+void handle_sigchld(int sig){
+	printf("My child has died, either naturally, or was killed\n");
+}
+
 int main(void){
 
-	signal(SIGINT, handle_sigint);
-	signal(SIGABRT, handle_sigabrt);
-	while(1==1){
-		printf("hello world\n");
-		sleep(1);
+	int pid;
+
+	if((pid = fork()) > 0){
+		// Parent does this
+		signal(SIGABRT, handle_sigabrt);
+		signal(SIGCHLD, handle_sigchld);
+		while(1==1){
+			printf("My Child is %d\n", pid);
+			sleep(1);
+		}
+	}
+	else if(pid == 0){
+		// Child does this
+		signal(SIGINT, handle_sigint);
+		sleep(10);
+		exit(EXIT_SUCCESS);
+	}
+	else{
+		fprintf(stderr, "Error creating child\n");
 	}
 
 
-	return 0;
+	return EXIT_SUCCESS;
 }
